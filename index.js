f9af997c1bab20603071cc1cdeceab6a36162a4a
davidwarrington <davidmwarrington+github@gmail.com>
2020-11-06 20:53:00 +0000

    Add Webpack and restart on "add" and "unlink" FS events.

---
+++ index.js
@@ -0,0 +1,22 @@
+const chokidar = require('chokidar');
+const Builder = require('./packages/Builder');
+const TEMPLATES_DIR = 'src/templates/';
+const builder = new Builder();
+
+(async () => {
+    await builder.start('watch');
+
+    chokidar.watch('./src', { ignoreInitial: true }).on('all', async (event, path) => {
+        const changeInTemplatesDir = path.startsWith(TEMPLATES_DIR);
+        const restartEvent = ['add', 'unlink'].includes(event);
+        const builderIsAlreadyRestarting = builder.state === Builder.states.RESTARTING;
+
+        if (
+            changeInTemplatesDir &&
+            restartEvent &&
+            !builderIsAlreadyRestarting
+        ) {
+            builder.restart('watch');
+        }
+    });
+})();
---
+++ packages/Builder.js
@@ -0,0 +1,67 @@
+const consola = require('consola');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config');
+
+const STATES = {
+    IDLE: 1,
+    RUNNING: 2,
+    RESTARTING: 3,
+};
+
+/** @typedef {'run'|'watch'} RunMode */
+
+class Builder {
+    constructor() {
+        this.compiler = null;
+        this.state = STATES.IDLE;
+    }
+
+    /** @param {RunMode} mode */
+    async start(mode) {
+        // Close existing compiler if already running.
+        await this.close();
+
+        this.compiler = webpack(webpackConfig);
+
+        if (mode === 'run') {
+            this.state = STATES.RUNNING;
+            this.compiler.run(error => {
+                if (error) {
+                    consola.error(new Error(error));
+                }
+
+                this.state = STATES.IDLE;
+                consola.success('Built!');
+            });
+        } else if (mode === 'watch') {
+            this.compiler.watch({}, error => {
+                if (error) {
+                    consola.error(new Error(error));
+                }
+                this.state = STATES.RUNNING;
+                consola.success('Successfully built!');
+            })
+        }
+    }
+
+    async close() {
+        if (!this.compiler) {
+            this.state = STATES.IDLE;
+            return Promise.resolve();
+        }
+
+        this.compiler.close(() => {
+            this.state = STATES.IDLE;
+            Promise.resolve();
+        });
+    }
+
+    /** @param {RunMode} mode */
+    async restart(mode) {
+        consola.info('Restarting...');
+        this.state = STATES.RESTARTING;
+        this.start(mode);
+    }
+}
+
+module.exports = Builder;
