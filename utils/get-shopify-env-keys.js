58a6e4f1b1513039a205973215aa70410d2a57d3
davidwarrington <davidmwarrington+github@gmail.com>
2020-12-19 18:00:20 +0000

    Add support for named env files.

--- utils/get-shopify-env-keys.js
+++ utils/get-shopify-env-keys.js
@@ -1,9 +1,35 @@
+const fs = require('fs');
+const path = require('path');
 const dotenv = require('dotenv');
 const convertSnakeToCamelCase = require('./convert-snake-to-camel-case');
 
-/** @returns {{[key: string]: string}} */
-const getShopifyEnvKeys = () => {
-    const results = dotenv.config();
+/**
+ * @param {string} name
+ *
+ * Function is a copy of _getFileName from {@link https://github.com/Shopify/slate/blob/5ecc510efed9a14fa9e3954c1adedeac91650144/packages/slate-env/index.js#L37|@shopify/slate-env}.
+ */
+const getEnvFileName = name => {
+    const basename = '.env';
+
+    if (typeof name === 'undefined' || name.trim() === '') {
+        return basename;
+    }
+
+    return `${basename}.${name}`;
+};
+
+/**
+ * @param {string} env
+ * @returns {{[key: string]: string}}
+ */
+const getShopifyEnvKeys = env => {
+    const envFilePath = path.resolve(process.cwd(), getEnvFileName(env));
+
+    if (!fs.existsSync(envFilePath)) {
+        throw new Error(`Missing env file at ${envFilePath}`);
+    }
+
+    const results = dotenv.config({ path: envFilePath });
     const shopifyEntries = Object.entries(results.parsed)
         .filter(([key]) => key.startsWith('SHOPIFY_'))
         .map(([key, value]) => [
