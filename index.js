b351999546af6375f92ba49960ad605dd311c0ce
David Warrington <davidmwarrington+github@gmail.com>
2020-12-20 13:39:53 +0000

    Add Theme Kit for syncing theme changes. (#4)

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const chokidar = require('chokidar');
+const consola = require('consola');
+const themeKit = require('@shopify/themekit');
+const yargs = require('yargs');
 const Builder = require('./packages/Builder');
+const getShopifyEnvKeys = require('./utils/get-shopify-env-keys');
 
+const { env } = yargs.argv;
 const LAYOUT_DIR = 'src/scripts/layout/';
 const TEMPLATES_DIR = 'src/scripts/templates/';
 const builder = new Builder();
 
 (async () => {
     await builder.start('watch');
+
+    try {
+        themeKit.command('watch', {
+            dir: 'dist',
+            ...getShopifyEnvKeys(env),
+        });
+    } catch (error) {
+        consola.error(error);
+    }
 
     chokidar
         .watch('./src', { ignoreInitial: true })
