3fd75bceaf38d99cd1c56650950b861033f50d0e
davidwarrington <davidmwarrington+github@gmail.com>
2020-12-22 21:12:24 +0000

    Allow theme to build before running deploy, and register commands via yargs API.

--- cli.js
+++ cli.js
@@ -1,42 +1,21 @@
-const consola = require('consola');
 const yargs = require('yargs');
 const build = require('./commands/build');
 const deploy = require('./commands/deploy');
 const watch = require('./commands/watch');
 
-const { argv } = yargs;
-
-const commands = { build, deploy, watch };
-
-const {
-    _: { 0: command },
-    ...args
-} = argv;
-
 (() => {
-    const noCommandFoundMessage = `Please try running one of the following commands: ${Object.keys(
-        commands
-    ).join(', ')}.`;
-
-    if (typeof command === 'undefined') {
-        consola.error(
-            new Error(`No command provided. ${noCommandFoundMessage}`)
-        );
-        return;
-    }
-
-    if (!commands[command]) {
-        consola.error(
-            new Error(
-                `Command "${command}" not found. ${noCommandFoundMessage}`
-            )
-        );
-        return;
-    }
-
-    try {
-        commands[command](args);
-    } catch (error) {
-        consola.error(error);
-    }
+    return yargs
+        .command('build', 'Build theme from source.', {}, build)
+        .command(
+            'deploy [env]',
+            'Build theme and deploy to given environments.',
+            {},
+            deploy
+        )
+        .command(
+            'watch [env]',
+            'Watch source files for theme updates.',
+            {},
+            watch
+        ).argv;
 })();
--- commands/deploy.js
+++ commands/deploy.js
@@ -6,17 +6,20 @@ const getShopifyEnvKeys = require('../utils/get-shopify-env-keys');
 const deploy = async ({ env }) => {
     try {
         await build();
+        consola.info('Starting deployment');
 
         // Ensure environments are in an array so that we can chain deployments
         const envs = Array.isArray(env) ? env : [env];
 
-        envs.reduce(async (previousPromise, currentEnv) => {
+        await envs.reduce(async (previousPromise, currentEnv) => {
             await previousPromise;
             return themeKit.command('deploy', {
                 dir: 'dist',
                 ...getShopifyEnvKeys(currentEnv),
             });
         }, Promise.resolve());
+
+        consola.success('Finished deployment.');
     } catch (error) {
         consola.error(error);
     }
