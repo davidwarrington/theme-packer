aa13511deb424289f420576ba7fa60827572e220
davidwarrington <davidmwarrington+github@gmail.com>
2021-01-27 21:36:28 +0000

    Change config name to theme-packer.config.js.

--- utils/get-user-config.js
+++ utils/get-user-config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 /** @returns {object} */
 const getUserConfig = () => {
-    const configPath = path.resolve(process.cwd(), 'shopify.config.js');
+    const configPath = path.resolve(process.cwd(), 'theme-packer.config.js');
 
     if (fs.existsSync(configPath)) {
         // eslint-disable-next-line global-require, import/no-dynamic-require
